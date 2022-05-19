e079cb0c582397699ae5b641386d67db5b6c1c6d
Michael Nelson <zsc.michaeln@gmail.com>
2022-05-19 13:58:04 -0500

    Create p2_code.js

---
+++ javascript/p2_code.js
@@ -0,0 +1,26 @@
+// Initialize form fields with variables
+var sName = "SoldierName";
+var ssn = "LastFourSSN";
+var sSign = "SoldierSignature";
+var dSign = "DateSigned";
+
+// Create new soldier entry
+//soldier[i].sName
+
+var soldier = new Array();
+
+soldier[0] = {
+    sName:"Michael Nelson",
+    ssn: 3723,
+    sSign: false,
+    dSign: ""
+}
+soldier[1] = {
+    sName:"Joe Snuffy",
+    ssn: 4856,
+    sSign: false,
+    dSign: ""
+}
+
+//alert(soldier[0].sName);
+alert()
